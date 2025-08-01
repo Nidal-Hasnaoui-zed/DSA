fc4d0b918942ba7c2293e079ef1cb569dee456f3
Hairo <hasnaouinidal308@gmail.com>
2025-08-01 17:49:39 +0100

    this is the last test for me in selection algorithems bro !

--- Arrays/test.py
+++ Arrays/test.py
@@ -1,10 +1,14 @@
-# Bubble sort alogorihtme !
+# Selection sort alogorihtme !
 
 my_array = [4,3,2,1] 
 n = len(my_array)
 for i in range(n-1): 
-    for j in range(n-i-1): 
-        if my_array[j] > my_array [j+1] : 
-            my_array[j] , my_array [j+1] = my_array[j+1] , my_array[j]
-            
-print(f'the sorted array is {my_array}')
+    min_index = i
+    for j in range(i+1,n):
+        if my_array[j] < my_array[min_index] : 
+            min_index = j
+    # min_value = my_array.pop(min_index)
+    # my_array.insert(i,min_value)
+    my_array[i],my_array[min_index] = my_array[min_index] , my_array[i]
+    
+print(f'the sorted arraya is {my_array}')
