22a8ffce245077f9be97edc0a1db262e4afb87c6
Hairo <hasnaouinidal308@gmail.com>
2025-08-01 17:10:13 +0100

    this is the code of selection sort Algorithems !

--- Arrays/SelectionSort.py
+++ Arrays/SelectionSort.py
@@ -1,12 +1,12 @@
-# lets see  my test 
-
+# okey i understand this bro : 
 my_array = [4,3,2,1]
-min_val = my_array[0]
-n = len(my_array)
+n= len(my_array)
 for i in range(n-1): 
-    for k in range(n) : 
-        if my_array[k] < min_val : 
-            # and here i was lost again !
+    min_index = i 
+    for j in range(i+1, n): 
+        if my_array[j] < my_array[min_index] : 
+            min_index = j 
+    min_value = my_array.pop(min_index)
+    my_array.insert(i,min_value)
 
-            
-            
+print(f'the sorted array is readu {my_array}')
--- Arrays/tempCodeRunnerFile.py
+++ Arrays/tempCodeRunnerFile.py
@@ -1,8 +1,1 @@
-my_array = [4,3,2,1]
-min_val = my_array[0]
-n = len(my_array)
-for i in range(n) : 
-    if my_array[i] < min_val : 
-        min_val = my_array[i]
-print(f'min val is {min_val}') 
-            
+print(f'the sorted array is readu {my_array}')
