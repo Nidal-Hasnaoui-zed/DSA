8db18f29653dbca77f60d656a533a0e69052f207
Hairo <hasnaouinidal308@gmail.com>
2025-08-03 12:07:27 +0100

    understanding the selection algo bro in python !

--- revise/revise.py
+++ revise/revise.py
@@ -1,9 +1,12 @@
-#we will learn the bubblt sore algo : 
+# we will learn the selection sort algo in this momrnt !
 l = [4,3,2,1]
 n = len(l)
 for i in range(n-1): 
-    for j in range(n-i-1): 
-        if l[j] > l[j+1] : 
-            l[j] , l[j+1] = l[j+1] , l[j]
+    min_index = i 
+    for j in range(i+1, n):
+        if l[j] < l[min_index] : 
+            min_index = j 
+    min_value = l.pop(min_index)
+    l.insert(i,min_value)
 
-print(f'the sorted array is {l}')
+print(f'the soreted arrays is this one {l}')            
